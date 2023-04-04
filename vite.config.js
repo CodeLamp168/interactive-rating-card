bbbadfa5a932a0bbc52dce54013730efde9805b3
Caleb D. Sim <103973059+CodeLamp168@users.noreply.github.com>
2023-04-04 10:58:24 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Interactive_Rating_Component_React_Vite",
+  base: "interactive-rating-card",
   plugins: [react()],
 })
